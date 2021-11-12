4e27a2ab563fdac87d6de9aa837a4bd6f9a2b8ea
Rollczi <ndejlich5@gmail.com>
2021-11-12 14:44:13 +0100

    Update AnnotationParser documentation.

--- litecommands-core/src/main/java/dev/rollczi/litecommands/annotations/parser/AnnotationParser.java
+++ litecommands-core/src/main/java/dev/rollczi/litecommands/annotations/parser/AnnotationParser.java
@@ -1,18 +1,22 @@
 package dev.rollczi.litecommands.annotations.parser;
 
+import dev.rollczi.litecommands.annotations.IgnoreClass;
+import dev.rollczi.litecommands.annotations.IgnoreMethod;
 import dev.rollczi.litecommands.component.ScopeMetaData;
 import panda.std.Option;
 
 import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Method;
 
 public interface AnnotationParser {
 
     /**
      * Annotation parser
      *
-     * @throws IllegalArgumentException when annotatedElement isn't instance of Method or Class
-     * @param annotatedElement instance of Method or Class
-     * @return information contained in annotatedElement's annotations
+     * @param annotatedElement instance of {@link Method} or {@link Class}
+     * @return optional information contained in annotatedElement's annotation;
+     *      empty option if annotatedElement isn't instance of {@link Method} or {@link Class};
+     *      empty option if annotatedElement contains annotation {@link IgnoreMethod} or {@link IgnoreClass}.
      */
     Option<ScopeMetaData> parse(AnnotatedElement annotatedElement);
 
