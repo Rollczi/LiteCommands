593028c38363f1668df1ca1fee9c7b2bc657401d
Rollczi <ndejlich5@gmail.com>
2022-02-26 21:40:51 +0100

    Fix import typo.

--- litecommands-core/src/main/java/dev/rollczi/litecommands/valid/handle/ExecutionResultHandler.java
+++ litecommands-core/src/main/java/dev/rollczi/litecommands/valid/handle/ExecutionResultHandler.java
@@ -1,6 +1,7 @@
 package dev.rollczi.litecommands.valid.handle;
 
 import dev.rollczi.litecommands.component.ExecutionResult;
+
 public interface ExecutionResultHandler {
 
     void handle(ExecutionResult executionResult);
