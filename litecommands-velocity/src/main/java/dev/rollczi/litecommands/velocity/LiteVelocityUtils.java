f46d1d8f4898048da0ae82ad4b034bd18d6dbbd6
Osnixer <Osnixeroo@gmail.com>
2022-04-26 23:17:36 +0200

    RollcziGH-68 💎 Fix tabcompleter for velocity implementation

--- litecommands-velocity/src/main/java/dev/rollczi/litecommands/velocity/LiteVelocityUtils.java
+++ litecommands-velocity/src/main/java/dev/rollczi/litecommands/velocity/LiteVelocityUtils.java
@@ -4,13 +4,28 @@ import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.command.SimpleCommand;
 import dev.rollczi.litecommands.LiteInvocation;
 import dev.rollczi.litecommands.platform.LiteSenderCreator;
+import panda.utilities.StringUtils;
 
 public final class LiteVelocityUtils {
 
-    private LiteVelocityUtils() {}
+    private LiteVelocityUtils() {
+
+    }
 
     public static LiteInvocation adaptInvocation(String name, SimpleCommand.Invocation invocation, LiteSenderCreator<CommandSource> creator) {
         return new LiteInvocation(name, invocation.alias(),  creator.create(invocation.source()), invocation.arguments());
     }
+
+    public static LiteInvocation adaptSuggestInvocation(String name, SimpleCommand.Invocation invocation, LiteSenderCreator<CommandSource> creator) {
+        String[] arguments = invocation.arguments();
+
+        if (arguments.length == 0) {
+            arguments = new String[] { StringUtils.EMPTY };
+        }
+
+        return new LiteInvocation(name, invocation.alias(),  creator.create(invocation.source()), arguments);
+    }
+
+
 
 }
