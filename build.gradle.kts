639d050ea785ed6995e98ec83cc963a73657a5c1
Rollczi <ndejlich5@gmail.com>
2022-05-10 17:53:46 +0200

    Release 2.0.0-pre2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 allprojects {
     group = "dev.rollczi"
-    version = "2.0.0-pre1"
+    version = "2.0.0-pre2"
 
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
