b30df0fe0d1eb6238e7e6b582ca0333b7aae3b99
Rollczi <ndejlich5@gmail.com>
2022-05-14 19:15:48 +0200

    2.0.0-pre8 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 allprojects {
     group = "dev.rollczi.litecommands"
-    version = "2.0.0-pre7"
+    version = "2.0.0-pre8"
 
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
