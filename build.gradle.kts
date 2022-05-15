dc486d39b624e1492fc4025d96c0189046e04f9e
Rollczi <ndejlich5@gmail.com>
2022-05-16 01:33:40 +0200

    2.0.0-pre9 Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 allprojects {
     group = "dev.rollczi.litecommands"
-    version = "2.0.0-pre8"
+    version = "2.0.0-pre9"
 
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
