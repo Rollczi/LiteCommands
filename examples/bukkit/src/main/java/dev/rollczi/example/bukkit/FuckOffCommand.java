781f44209b04177088c26bbb767f81ba1afa0940
Sky0x <100078621+Skyy0x@users.noreply.github.com>
2022-07-19 14:45:01 +0200

    Update FuckOffCommand.java

--- examples/bukkit/src/main/java/dev/rollczi/example/bukkit/FuckOffCommand.java
+++ examples/bukkit/src/main/java/dev/rollczi/example/bukkit/FuckOffCommand.java
@@ -4,6 +4,7 @@ import dev.rollczi.example.bukkit.util.ChatUtil;
 import dev.rollczi.litecommands.argument.Arg;
 import dev.rollczi.litecommands.argument.Name;
 import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 import dev.rollczi.litecommands.argument.joiner.Joiner;
 import dev.rollczi.litecommands.command.execute.Execute;
 import dev.rollczi.litecommands.command.permission.Permission;
@@ -16,6 +17,7 @@ public class FuckOffCommand {
     @Execute(min = 2)
     public void banPlayer(final Player sender, @Arg @Name("target") final Player target, @Joiner final String reason){
         target.kickPlayer(ChatUtil.color(reason));
+        Bukkit.broadcastMessage(ChatUtil.color("&cPlayer &7" + target.getName() + " &cgot fucked up!"));
     }
 
 }
