88dfb3ee596f72338e4d1af71b421b2014d5b83a
Rollczi <ndejlich5@gmail.com>
2022-10-22 20:09:33 +0200

    Release 2.6.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 allprojects {
     group = "dev.rollczi.litecommands"
-    version = "2.6.0"
+    version = "2.6.0-SNAPSHOT"
 
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
