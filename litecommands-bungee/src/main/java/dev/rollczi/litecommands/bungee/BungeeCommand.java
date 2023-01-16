5054d3515f88a7128909a295ff79547d38e2119a
pauhull <pauhull3@gmail.com>
2023-01-16 23:15:28 +0100

    GH-163 Add native permission support and implement OnlyPlayerContextuals for Velocity and Bungee  (#163)
    * Add native permission handling for Velocity
    
    * Add VelocityOnlyPlayerContextual and BungeeOnlyPlayerContextual
    
    * Add native permission handling for Bungee
    
    * Fix code style.
    
    Co-authored-by: Rollczi <ndejlich5@gmail.com>

--- litecommands-bungee/src/main/java/dev/rollczi/litecommands/bungee/BungeeCommand.java
+++ litecommands-bungee/src/main/java/dev/rollczi/litecommands/bungee/BungeeCommand.java
@@ -1,6 +1,7 @@
 package dev.rollczi.litecommands.bungee;
 
 import dev.rollczi.litecommands.command.LiteInvocation;
+import dev.rollczi.litecommands.command.permission.RequiredPermissions;
 import dev.rollczi.litecommands.command.section.CommandSection;
 import dev.rollczi.litecommands.platform.ExecuteListener;
 import dev.rollczi.litecommands.platform.SuggestionListener;
@@ -13,22 +14,34 @@ class BungeeCommand extends Command implements TabExecutor {
     private final CommandSection<CommandSender> commandSection;
     private final ExecuteListener<CommandSender> executeListener;
     private final SuggestionListener<CommandSender> suggestionListener;
+    private final boolean nativePermissions;
 
-    public BungeeCommand(CommandSection<CommandSender> command, ExecuteListener<CommandSender> executeListener, SuggestionListener<CommandSender> suggestionListener) {
+    public BungeeCommand(CommandSection<CommandSender> command, ExecuteListener<CommandSender> executeListener, SuggestionListener<CommandSender> suggestionListener, boolean nativePermissions) {
         super(command.getName(), "", command.getAliases().toArray(new String[0]));
         this.commandSection = command;
         this.executeListener = executeListener;
         this.suggestionListener = suggestionListener;
+        this.nativePermissions = nativePermissions;
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        this.executeListener.execute(sender, new LiteInvocation(new BungeeSender(sender), commandSection.getName(), commandSection.getName(), args));
+        this.executeListener.execute(sender, new LiteInvocation(new BungeeSender(sender), this.commandSection.getName(), this.commandSection.getName(), args));
     }
 
     @Override
     public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
-        return suggestionListener.suggest(sender, new LiteInvocation(new BungeeSender(sender), commandSection.getName(), commandSection.getName(), args)).multilevelSuggestions();
+        return this.suggestionListener.suggest(sender, new LiteInvocation(new BungeeSender(sender), this.commandSection.getName(), this.commandSection.getName(), args)).multilevelSuggestions();
     }
 
+    @Override
+    public boolean hasPermission(CommandSender sender) {
+
+        if (this.nativePermissions) {
+            RequiredPermissions requiredPermissions = RequiredPermissions.of(this.commandSection.meta(), new BungeeSender(sender));
+            return requiredPermissions.isEmpty();
+        }
+
+        return super.hasPermission(sender);
+    }
 }
