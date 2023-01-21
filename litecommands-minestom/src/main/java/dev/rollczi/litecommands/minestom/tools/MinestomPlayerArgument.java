368dc6e6a948aad49fa5dd6fff499b655f0f0da6
Rollczi <ndejlich5@gmail.com>
2023-01-22 00:28:14 +0100

    Don't use ConnectionManager from MinecraftServer.getConnectionManager() in MinestomPlayerArgument.

--- litecommands-minestom/src/main/java/dev/rollczi/litecommands/minestom/tools/MinestomPlayerArgument.java
+++ litecommands-minestom/src/main/java/dev/rollczi/litecommands/minestom/tools/MinestomPlayerArgument.java
@@ -4,8 +4,8 @@ import dev.rollczi.litecommands.argument.ArgumentName;
 import dev.rollczi.litecommands.argument.simple.OneArgument;
 import dev.rollczi.litecommands.command.LiteInvocation;
 import dev.rollczi.litecommands.suggestion.Suggestion;
-import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
+import net.minestom.server.network.ConnectionManager;
 import panda.std.Option;
 import panda.std.Result;
 
@@ -15,20 +15,22 @@ import java.util.stream.Collectors;
 @ArgumentName("player")
 public class MinestomPlayerArgument<MESSAGE> implements OneArgument<Player> {
 
+    private final ConnectionManager connectionManager;
     private final MESSAGE playerNotFoundMessage;
 
-    public MinestomPlayerArgument(MESSAGE playerNotFoundMessage) {
+    public MinestomPlayerArgument(ConnectionManager connectionManager, MESSAGE playerNotFoundMessage) {
+        this.connectionManager = connectionManager;
         this.playerNotFoundMessage = playerNotFoundMessage;
     }
 
     @Override
     public Result<Player, Object> parse(LiteInvocation invocation, String argument) {
-        return Option.of(MinecraftServer.getConnectionManager().getPlayer(argument)).toResult(playerNotFoundMessage);
+        return Option.of(this.connectionManager.getPlayer(argument)).toResult(this.playerNotFoundMessage);
     }
 
     @Override
     public List<Suggestion> suggest(LiteInvocation invocation) {
-        return MinecraftServer.getConnectionManager().getOnlinePlayers().stream()
+        return this.connectionManager.getOnlinePlayers().stream()
                 .map(Player::getUsername)
                 .map(Suggestion::of)
                 .collect(Collectors.toList());
