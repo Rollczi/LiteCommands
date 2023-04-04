859cb074d2414e3b2feafc7da2cfc3be018ba6b9
Rollczi <ndejlich5@gmail.com>
2023-04-04 18:48:24 +0200

    Fix handling of invalid boolean argument parsing.

--- litecommands-core/src/main/java/dev/rollczi/litecommands/argument/basictype/BooleanArgument.java
+++ litecommands-core/src/main/java/dev/rollczi/litecommands/argument/basictype/BooleanArgument.java
@@ -12,7 +12,7 @@ public class BooleanArgument extends AbstractBasicTypeArgument<Boolean> {
         return Option.of(argument)
             .filter(arg -> arg.equalsIgnoreCase("true") || arg.equalsIgnoreCase("false"))
             .map(Boolean::parseBoolean)
-            .orThrow(() -> new IllegalArgumentException("Argument is not boolean"));
+            .orThrow(() -> new NumberFormatException("Argument is not boolean"));
     }
 
 }
