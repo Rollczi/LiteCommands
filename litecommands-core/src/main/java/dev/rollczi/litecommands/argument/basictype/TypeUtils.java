76e50a2d0d0134627ead69932edb5bab4c6f6c66
Rollczi <ndejlich5@gmail.com>
2023-05-10 16:53:31 +0200

    Remove default "text" string argument suggestion.

--- litecommands-core/src/main/java/dev/rollczi/litecommands/argument/basictype/TypeUtils.java
+++ litecommands-core/src/main/java/dev/rollczi/litecommands/argument/basictype/TypeUtils.java
@@ -13,11 +13,11 @@ import java.util.function.Supplier;
 
 final class TypeUtils {
 
-    static final String[] NUMBER_SHORT_SUGGESTION = { "0", "1", "5", "10", "50" };
-    static final String[] NUMBER_SUGGESTION = { "0", "1", "5", "10", "50", "100", "500" };
+    static final String[] NUMBER_SHORT_SUGGESTION = { "0", "1", "5", "10", "25", "50" };
+    static final String[] NUMBER_SUGGESTION = { "0", "1", "5", "10", "25", "50", "100", "250", "500" };
     static final String[] DECIMAL_SUGGESTION = { "0", "1", "1.5", "10", "10.5", "100", "100.5" };
 
-    static final String[] STRING_SUGGESTION = { "text" };
+    static final String[] STRING_SUGGESTION = {};
     static final String[] CHARACTER_SUGGESTION = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
     static final String[] BOOLEAN_SUGGESTION = { "true", "false" };
