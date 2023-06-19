64e8b898d223a7ac011ee3962edd64f47d7db767
Rollczi <ndejlich5@gmail.com>
2023-06-19 14:11:22 +0200

    Add time parser support.

---
+++ litecommands-core/src/dev/rollczi/litecommands/time/DurationParser.java
@@ -0,0 +1,54 @@
+package dev.rollczi.litecommands.time;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+
+public class DurationParser extends TemporalAmountParser<Duration> {
+
+    public static final TemporalAmountParser<Duration> TIME_UNITS = new DurationParser()
+        .withUnit("ms", ChronoUnit.MILLIS)
+        .withUnit("s", ChronoUnit.SECONDS)
+        .withUnit("m", ChronoUnit.MINUTES)
+        .withUnit("h", ChronoUnit.HOURS);
+
+    public static final TemporalAmountParser<Duration> DATE_TIME_UNITS = new DurationParser()
+        .withUnit("ns", ChronoUnit.NANOS)
+        .withUnit("us", ChronoUnit.MICROS)
+        .withUnit("ms", ChronoUnit.MILLIS)
+        .withUnit("s", ChronoUnit.SECONDS)
+        .withUnit("m", ChronoUnit.MINUTES)
+        .withUnit("h", ChronoUnit.HOURS)
+        .withUnit("d", ChronoUnit.DAYS)
+        .withUnit("w", ChronoUnit.WEEKS)
+        .withUnit("mo", ChronoUnit.MONTHS)
+        .withUnit("y", ChronoUnit.YEARS);
+
+    public DurationParser() {
+        super(LocalDateTimeProvider.now());
+    }
+
+    public DurationParser(LocalDateTimeProvider localDateTimeProvider) {
+        super(localDateTimeProvider);
+    }
+
+    private DurationParser(Map<String, ChronoUnit> units, LocalDateTimeProvider baseForTimeEstimation) {
+        super(units, baseForTimeEstimation);
+    }
+
+    @Override
+    protected TemporalAmountParser<Duration> clone(Map<String, ChronoUnit> units, LocalDateTimeProvider baseForTimeEstimation) {
+        return new DurationParser(units, baseForTimeEstimation);
+    }
+
+    @Override
+    protected Duration toTempolarAmount(LocalDateTimeProvider baseForTimeEstimation, Duration duration) {
+        return duration;
+    }
+
+    @Override
+    protected Duration toDuration(LocalDateTimeProvider baseForTimeEstimation, Duration temporalAmount) {
+        return temporalAmount;
+    }
+
+}
---
+++ litecommands-core/src/dev/rollczi/litecommands/time/PeriodParser.java
@@ -0,0 +1,50 @@
+package dev.rollczi.litecommands.time;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+
+public class PeriodParser extends TemporalAmountParser<Period> {
+
+    public static final TemporalAmountParser<Period> DATE_UNITS = new PeriodParser()
+        .withUnit("d", ChronoUnit.DAYS)
+        .withUnit("w", ChronoUnit.WEEKS)
+        .withUnit("mo", ChronoUnit.MONTHS)
+        .withUnit("y", ChronoUnit.YEARS);
+
+    public PeriodParser() {
+        super(LocalDateTimeProvider.now());
+    }
+
+    public PeriodParser(LocalDateTimeProvider baseForTimeEstimation) {
+        super(baseForTimeEstimation);
+    }
+
+    private PeriodParser(Map<String, ChronoUnit> units, LocalDateTimeProvider baseForTimeEstimation) {
+        super(units, baseForTimeEstimation);
+    }
+
+    @Override
+    protected TemporalAmountParser<Period> clone(Map<String, ChronoUnit> units, LocalDateTimeProvider baseForTimeEstimation) {
+        return new PeriodParser(units, baseForTimeEstimation);
+    }
+
+    @Override
+    protected Period toTempolarAmount(LocalDateTimeProvider basisEstimation, Duration duration) {
+        LocalDateTime localDate = basisEstimation.get();
+        LocalDateTime estimatedDate = localDate.plus(duration);
+
+        return Period.between(localDate.toLocalDate(), estimatedDate.toLocalDate());
+    }
+
+    @Override
+    protected Duration toDuration(LocalDateTimeProvider basisEstimation, Period period) {
+        LocalDateTime localDate = basisEstimation.get();
+        LocalDateTime estimatedDate = localDate.plus(period);
+
+        return Duration.between(localDate, estimatedDate);
+    }
+
+}
