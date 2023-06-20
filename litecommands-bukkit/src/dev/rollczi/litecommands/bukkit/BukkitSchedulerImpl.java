68879167047be8785852957c646fc84acfad7938
Rollczi <ndejlich5@gmail.com>
2023-06-20 17:53:12 +0200

    Add bukkit scheduler impl.

---
+++ litecommands-bukkit/src/dev/rollczi/litecommands/bukkit/BukkitSchedulerImpl.java
@@ -0,0 +1,56 @@
+package dev.rollczi.litecommands.bukkit;
+
+import dev.rollczi.litecommands.scheduler.Scheduler;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+class BukkitSchedulerImpl implements Scheduler {
+
+    private final BukkitScheduler bukkitScheduler;
+    private final Plugin plugin;
+
+    BukkitSchedulerImpl(BukkitScheduler bukkitScheduler, Plugin plugin) {
+        this.bukkitScheduler = bukkitScheduler;
+        this.plugin = plugin;
+    }
+
+    @Override
+    public <T> CompletableFuture<T> supplySync(Supplier<T> supplier) {
+        CompletableFuture<T> future = new CompletableFuture<>();
+
+        bukkitScheduler.runTask(plugin, () -> {
+            try {
+                future.complete(supplier.get());
+            }
+            catch (Throwable throwable) {
+                future.completeExceptionally(throwable);
+            }
+        });
+
+        return future;
+    }
+
+    @Override
+    public <T> CompletableFuture<T> supplyAsync(Supplier<T> supplier) {
+        CompletableFuture<T> future = new CompletableFuture<>();
+
+        bukkitScheduler.runTaskAsynchronously(plugin, () -> {
+            try {
+                future.complete(supplier.get());
+            }
+            catch (Throwable throwable) {
+                future.completeExceptionally(throwable);
+            }
+        });
+
+        return future;
+    }
+
+    @Override
+    public void shutdown() {
+    }
+
+}
--- litecommands-bukkit/src/dev/rollczi/litecommands/bukkit/LiteCommandsBukkit.java
+++ litecommands-bukkit/src/dev/rollczi/litecommands/bukkit/LiteCommandsBukkit.java
@@ -4,9 +4,12 @@ import dev.rollczi.litecommands.LiteCommandsFactory;
 import dev.rollczi.litecommands.builder.LiteCommandsBuilder;
 import dev.rollczi.litecommands.bukkit.tools.BukkitOnlyPlayerContextual;
 import dev.rollczi.litecommands.bukkit.tools.BukkitPlayerArgument;
+import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
 public final class LiteCommandsBukkit {
@@ -14,14 +17,19 @@ public final class LiteCommandsBukkit {
     private LiteCommandsBukkit() {
     }
 
-    public static LiteCommandsBuilder<CommandSender, LiteBukkitSettings, ?> builder(Server server) {
-        return builder(server, new LiteBukkitSettings(server));
+    public static LiteCommandsBuilder<CommandSender, LiteBukkitSettings, ?> builder() {
+        return builder(JavaPlugin.getProvidingPlugin(LiteCommandsBukkit.class), Bukkit.getServer());
     }
 
-    public static LiteCommandsBuilder<CommandSender, LiteBukkitSettings, ?> builder(Server server, LiteBukkitSettings settings) {
+    public static LiteCommandsBuilder<CommandSender, LiteBukkitSettings, ?> builder(Plugin plugin, Server server) {
+        return builder(plugin, server, new LiteBukkitSettings(server));
+    }
+
+    public static LiteCommandsBuilder<CommandSender, LiteBukkitSettings, ?> builder(Plugin plugin, Server server, LiteBukkitSettings settings) {
         return LiteCommandsFactory.builder(CommandSender.class, new BukkitPlatform(settings))
             .bind(Server.class, () -> server)
-            .bind(BukkitScheduler.class, server::getScheduler)
+            .bind(BukkitScheduler.class, () -> server.getScheduler())
+            .scheduler(new BukkitSchedulerImpl(server.getScheduler(), plugin))
 
             .argument(Player.class, new BukkitPlayerArgument<>(server, name -> "Player " + name + " is not online!")) // TODO WIKI GUIDE links
 
