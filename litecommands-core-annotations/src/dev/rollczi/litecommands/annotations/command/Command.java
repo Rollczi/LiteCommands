b7dc4ffd66cc31e0a67550bd29083a02b568a664
Rollczi <ndejlich5@gmail.com>
2023-08-26 21:07:31 +0200

    Add docs for @Command @RootCommand and @Execute

--- litecommands-core-annotations/src/dev/rollczi/litecommands/annotations/command/Command.java
+++ litecommands-core-annotations/src/dev/rollczi/litecommands/annotations/command/Command.java
@@ -5,12 +5,25 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ ElementType.TYPE, ElementType.METHOD })
+/**
+ * Is used to mark the class as a command.
+ * This annotation is required for the class to be registered.
+ *
+ * @see dev.rollczi.litecommands.annotations.execute.Execute
+ * @see dev.rollczi.litecommands.annotations.command.RootCommand
+ */
+@Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Command {
 
+    /**
+     * Represents the part of the command.
+     */
     String name();
 
+    /**
+     * Represents the aliases of the command.
+     */
     String[] aliases() default {};
 
 }
--- litecommands-core-annotations/src/dev/rollczi/litecommands/annotations/command/RootCommand.java
+++ litecommands-core-annotations/src/dev/rollczi/litecommands/annotations/command/RootCommand.java
@@ -1,14 +1,16 @@
 package dev.rollczi.litecommands.annotations.command;
 
-import dev.rollczi.litecommands.annotations.processor.AnnotationInvoker;
-import dev.rollczi.litecommands.annotations.processor.AnnotationProcessor;
-import dev.rollczi.litecommands.command.builder.CommandBuilder;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Is used to mark the class as a root command (without name).
+ * This annotation is required for the class to be registered.
+ * If you want to create a command with base name, use {@link Command} annotation.
+ * @see dev.rollczi.litecommands.annotations.command.Command
+ */
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RootCommand {
