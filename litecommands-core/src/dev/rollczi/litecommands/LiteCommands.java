18f67b52a0e557d1747b2d588c8f1821af3bb47b
Rollczi <ndejlich5@gmail.com>
2023-10-02 23:36:22 +0200

    Release 3.0.0-BETA-pre16

--- litecommands-core/src/dev/rollczi/litecommands/LiteCommands.java
+++ litecommands-core/src/dev/rollczi/litecommands/LiteCommands.java
@@ -4,7 +4,7 @@ import dev.rollczi.litecommands.command.CommandManager;
 
 public interface LiteCommands<SENDER> {
 
-    String VERSION = "3.0.0-BETA-pre15";
+    String VERSION = "3.0.0-BETA-pre16";
 
     CommandManager<SENDER> getCommandManager();
 
