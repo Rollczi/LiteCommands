6939cfbb118fff7141145dd7d0a2853195ece088
Michał Wojtas <80779749+CitralFlo@users.noreply.github.com>
2023-10-11 11:36:46 +0200

    GH-222 Add PeriodArgumentResolver and corresponding tests, remove unused imports. (#234)
    * Add PeriodArgumentResolver.java, add corresponding tests, remove unused imports.
    
    * Update litecommands-annotations/test/dev/rollczi/litecommands/annotations/argument/resolver/standard/PeriodArgumentTest.java
    
    ---------
    
    Co-authored-by: Norbert Dejlich <49173834+Rollczi@users.noreply.github.com>

---
+++ litecommands-annotations/test/dev/rollczi/litecommands/annotations/argument/resolver/standard/PeriodArgumentTest.java
@@ -0,0 +1,45 @@
+package dev.rollczi.litecommands.annotations.argument.resolver.standard;
+
+import dev.rollczi.litecommands.annotations.LiteTestSpec;
+import dev.rollczi.litecommands.annotations.argument.Arg;
+import dev.rollczi.litecommands.annotations.command.Command;
+import dev.rollczi.litecommands.annotations.execute.Execute;
+import org.junit.jupiter.api.Test;
+
+import java.time.Period;
+
+class PeriodArgumentTest extends LiteTestSpec {
+
+    @Command(name = "test")
+    static class TestCommand {
+
+        @Execute
+        Period test(@Arg Period period) {
+            return period;
+        }
+    }
+
+    @Test
+    void test() {
+        platform.execute("test 1d")
+            .assertSuccess(Period.ofDays(1));
+    }
+
+    @Test
+    void testInvalid() {
+        platform.execute("test 1acab")
+            .assertFailure();
+    }
+
+    @Test
+    void testMultipleValues() {
+        platform.execute("test 7y3w")
+            .assertSuccess();
+    }
+
+    @Test
+    void testReturn() {
+        platform.execute("test 7y4mo3d")
+            .assertSuccess(Period.ofYears(7).plus(Period.ofMonths(4)).plus(Period.ofDays(3)));
+    }
+}
--- litecommands-core/src/dev/rollczi/litecommands/argument/resolver/standard/PeriodArgumentResolver.java
+++ litecommands-core/src/dev/rollczi/litecommands/argument/resolver/standard/PeriodArgumentResolver.java
@@ -7,36 +7,33 @@ import dev.rollczi.litecommands.invalidusage.InvalidUsage;
 import dev.rollczi.litecommands.invocation.Invocation;
 import dev.rollczi.litecommands.suggestion.SuggestionContext;
 import dev.rollczi.litecommands.suggestion.SuggestionResult;
-import dev.rollczi.litecommands.time.DurationParser;
+import dev.rollczi.litecommands.time.PeriodParser;
 
-import java.time.Duration;
+import java.time.Period;
 import java.util.Arrays;
 import java.util.List;
 
-public class DurationArgumentResolver<SENDER> extends ArgumentResolver<SENDER, Duration> {
+public class PeriodArgumentResolver<SENDER> extends ArgumentResolver<SENDER, Period> {
 
     private static final List<String> SUGGESTIONS_LIST = Arrays.asList(
-        "1s", "5s", "10s", "30s",
-        "1m", "1m30s", "5m", "10m", "30m",
-        "1h", "5h", "10h",
-        "1d", "7d", "30d"
+        "1h", "3h", "7h",
+        "1d", "7d", "30d",
+        "1m", "3m", "6m", "1y", "5y"
     );
 
-
     @Override
-    protected ParseResult<Duration> parse(Invocation<SENDER> invocation, Argument<Duration> context, String argument) {
+    protected ParseResult<Period> parse(Invocation<SENDER> invocation, Argument<Period> context, String argument) {
         try {
-            Duration parse = DurationParser.DATE_TIME_UNITS.parse(argument);
+            Period parse = PeriodParser.DATE_UNITS.parse(argument);
 
             return ParseResult.success(parse);
-        }
-        catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             return ParseResult.failure(InvalidUsage.Cause.INVALID_ARGUMENT);
         }
     }
 
     @Override
-    public SuggestionResult suggest(Invocation<SENDER> invocation, Argument<Duration> argument, SuggestionContext context) {
+    public SuggestionResult suggest(Invocation<SENDER> invocation, Argument<Period> argument, SuggestionContext context) {
         return SuggestionResult.of(SUGGESTIONS_LIST);
     }
 }
