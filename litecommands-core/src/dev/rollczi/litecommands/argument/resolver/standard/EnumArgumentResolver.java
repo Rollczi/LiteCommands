10efa655fafcd7c8288b404f727698c4b223c71e
BlackBaroness <shoyurash2@gmail.com>
2023-10-13 15:28:59 +0300

    Implement suggestions

--- litecommands-core/src/dev/rollczi/litecommands/argument/resolver/standard/EnumArgumentResolver.java
+++ litecommands-core/src/dev/rollczi/litecommands/argument/resolver/standard/EnumArgumentResolver.java
@@ -8,7 +8,14 @@ import dev.rollczi.litecommands.invocation.Invocation;
 import dev.rollczi.litecommands.suggestion.SuggestionContext;
 import dev.rollczi.litecommands.suggestion.SuggestionResult;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@SuppressWarnings("rawtypes")
 public class EnumArgumentResolver<SENDER> extends ArgumentResolver<SENDER, Enum> {
+
+    private final Map<Class<?>, SuggestionResult> cachedEnumSuggestions = new ConcurrentHashMap<>();
 
     @Override
     protected ParseResult<Enum> parse(Invocation<SENDER> invocation, Argument<Enum> context, String argument) {
@@ -21,10 +28,18 @@ public class EnumArgumentResolver<SENDER> extends ArgumentResolver<SENDER, Enum>
 
     @Override
     public SuggestionResult suggest(Invocation<SENDER> invocation, Argument<Enum> argument, SuggestionContext context) {
-        return super.suggest(invocation, argument, context);
+        return cachedEnumSuggestions.computeIfAbsent(argument.getWrapperFormat().getParsedType(), clazz -> {
+            final Object[] enumConstants = clazz.getEnumConstants();
+            if (enumConstants == null || enumConstants.length == 0) {
+                return SuggestionResult.empty();
+            }
+
+
+            return Arrays.stream(enumConstants).map(Object::toString).collect(SuggestionResult.collector());
+        });
     }
 
-    @SuppressWarnings({"unchecked", "rawtypes"})
+    @SuppressWarnings("unchecked")
     private Enum getEnum(Class<? extends Enum> clazz, String name) throws IllegalArgumentException {
         return Enum.valueOf(clazz, name);
     }
