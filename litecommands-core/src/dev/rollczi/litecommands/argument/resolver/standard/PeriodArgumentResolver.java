5026ce8e4877ced88040ff98c0af8192a49daff9
Rafał Chomczyk <rchomczyk@icloud.com>
2023-10-20 18:28:59 +0200

    Add an argument resolver for temporal amounts

--- litecommands-core/src/dev/rollczi/litecommands/argument/resolver/standard/PeriodArgumentResolver.java
+++ litecommands-core/src/dev/rollczi/litecommands/argument/resolver/standard/PeriodArgumentResolver.java
@@ -1,19 +1,11 @@
 package dev.rollczi.litecommands.argument.resolver.standard;
 
-import dev.rollczi.litecommands.argument.Argument;
-import dev.rollczi.litecommands.argument.resolver.ArgumentResolver;
-import dev.rollczi.litecommands.argument.parser.ParseResult;
-import dev.rollczi.litecommands.invalidusage.InvalidUsage;
-import dev.rollczi.litecommands.invocation.Invocation;
-import dev.rollczi.litecommands.suggestion.SuggestionContext;
-import dev.rollczi.litecommands.suggestion.SuggestionResult;
 import dev.rollczi.litecommands.time.PeriodParser;
-
 import java.time.Period;
 import java.util.Arrays;
 import java.util.List;
 
-public class PeriodArgumentResolver<SENDER> extends ArgumentResolver<SENDER, Period> {
+public class PeriodArgumentResolver<SENDER> extends TemporalAmountArgumentResolver<SENDER, Period> {
 
     private static final List<String> SUGGESTIONS_LIST = Arrays.asList(
         "1h", "3h", "7h",
@@ -21,19 +13,7 @@ public class PeriodArgumentResolver<SENDER> extends ArgumentResolver<SENDER, Per
         "1m", "3m", "6m", "1y", "5y"
     );
 
-    @Override
-    protected ParseResult<Period> parse(Invocation<SENDER> invocation, Argument<Period> context, String argument) {
-        try {
-            Period parse = PeriodParser.DATE_UNITS.parse(argument);
-
-            return ParseResult.success(parse);
-        } catch (IllegalArgumentException e) {
-            return ParseResult.failure(InvalidUsage.Cause.INVALID_ARGUMENT);
-        }
-    }
-
-    @Override
-    public SuggestionResult suggest(Invocation<SENDER> invocation, Argument<Period> argument, SuggestionContext context) {
-        return SuggestionResult.of(SUGGESTIONS_LIST);
+    public PeriodArgumentResolver() {
+        super(PeriodParser.DATE_UNITS, () -> SUGGESTIONS_LIST);
     }
 }
---
+++ litecommands-core/src/dev/rollczi/litecommands/argument/resolver/standard/TemporalAmountArgumentResolver.java
@@ -0,0 +1,42 @@
+package dev.rollczi.litecommands.argument.resolver.standard;
+
+import static dev.rollczi.litecommands.argument.parser.ParseResult.failure;
+import static dev.rollczi.litecommands.argument.parser.ParseResult.success;
+import static dev.rollczi.litecommands.invalidusage.InvalidUsage.Cause.INVALID_ARGUMENT;
+
+import dev.rollczi.litecommands.argument.Argument;
+import dev.rollczi.litecommands.argument.parser.ParseResult;
+import dev.rollczi.litecommands.argument.resolver.ArgumentResolver;
+import dev.rollczi.litecommands.invocation.Invocation;
+import dev.rollczi.litecommands.suggestion.SuggestionContext;
+import dev.rollczi.litecommands.suggestion.SuggestionResult;
+import dev.rollczi.litecommands.time.TemporalAmountParser;
+import java.time.temporal.TemporalAmount;
+import java.util.List;
+import java.util.function.Supplier;
+
+class TemporalAmountArgumentResolver<SENDER, UNIT extends TemporalAmount>
+    extends ArgumentResolver<SENDER, UNIT> {
+
+    private final TemporalAmountParser<UNIT> parser;
+    private final Supplier<List<String>> suggestions;
+
+    protected TemporalAmountArgumentResolver(TemporalAmountParser<UNIT> parser, Supplier<List<String>> suggestions) {
+        this.parser = parser;
+        this.suggestions = suggestions;
+    }
+
+    @Override
+    protected ParseResult<UNIT> parse(Invocation<SENDER> invocation, Argument<UNIT> context, String argument) {
+        try {
+            return success(parser.parse(argument));
+        } catch (IllegalArgumentException exception) {
+            return failure(INVALID_ARGUMENT);
+        }
+    }
+
+    @Override
+    public SuggestionResult suggest(Invocation<SENDER> invocation, Argument<UNIT> argument, SuggestionContext context) {
+        return SuggestionResult.of(suggestions.get());
+    }
+}
