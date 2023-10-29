57b6ed13c8081ae3a81c6f28e872f372812a661a
Rollczi <ndejlich5@gmail.com>
2023-10-29 16:33:03 +0100

    Refactor time parsers in the argument resolver.

--- litecommands-core/src/dev/rollczi/litecommands/argument/resolver/standard/PeriodArgumentResolver.java
+++ litecommands-core/src/dev/rollczi/litecommands/argument/resolver/standard/PeriodArgumentResolver.java
@@ -8,9 +8,9 @@ import java.util.List;
 public class PeriodArgumentResolver<SENDER> extends TemporalAmountArgumentResolver<SENDER, Period> {
 
     private static final List<String> SUGGESTIONS_LIST = Arrays.asList(
-        "1h", "3h", "7h",
         "1d", "7d", "30d",
-        "1m", "3m", "6m", "1y", "5y"
+        "1mo", "3mo", "6mo",
+        "1y", "3y", "6y"
     );
 
     public PeriodArgumentResolver() {
