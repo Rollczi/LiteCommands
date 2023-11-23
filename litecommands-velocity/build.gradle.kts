77d82dbca6a6362a89c4534aea704fed648e251a
Rafał Chomczyk <rchomczyk@icloud.com>
2023-11-23 17:44:59 +0100

    Add support for use of LiteCommands on Velocity with JDK 11

--- litecommands-velocity/build.gradle.kts
+++ litecommands-velocity/build.gradle.kts
@@ -21,6 +21,6 @@ publishing {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
