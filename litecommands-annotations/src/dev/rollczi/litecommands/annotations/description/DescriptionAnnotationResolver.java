65e2293ce4bf35f81e4713f8af8a8d2082a08f73
carm <carm@carm.cc>
2023-11-26 23:08:06 +0800

    DESCRIPTION meta to list

--- litecommands-annotations/src/dev/rollczi/litecommands/annotations/description/DescriptionAnnotationResolver.java
+++ litecommands-annotations/src/dev/rollczi/litecommands/annotations/description/DescriptionAnnotationResolver.java
@@ -4,12 +4,15 @@ import dev.rollczi.litecommands.annotations.AnnotationInvoker;
 import dev.rollczi.litecommands.annotations.AnnotationProcessor;
 import dev.rollczi.litecommands.meta.Meta;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class DescriptionAnnotationResolver<SENDER> implements AnnotationProcessor<SENDER> {
 
     @Override
     public AnnotationInvoker<SENDER> process(AnnotationInvoker<SENDER> invoker) {
         return invoker.on(Description.class, (annotation, metaHolder) -> {
-            metaHolder.meta().put(Meta.DESCRIPTION, annotation.value());
+            metaHolder.meta().put(Meta.DESCRIPTION, Arrays.stream(annotation.value()).collect(Collectors.toList()));
         });
     }
 
