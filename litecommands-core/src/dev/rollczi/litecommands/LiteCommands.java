84e77a4ab2fac2ee082eaf5788b896d7b8ad90c4
Rollczi <ndejlich5@gmail.com>
2023-12-10 23:40:24 +0100

    Add access to litecommands internal API.

--- litecommands-core/src/dev/rollczi/litecommands/LiteCommands.java
+++ litecommands-core/src/dev/rollczi/litecommands/LiteCommands.java
@@ -1,6 +1,7 @@
 package dev.rollczi.litecommands;
 
 import dev.rollczi.litecommands.command.CommandManager;
+import org.jetbrains.annotations.ApiStatus;
 
 public interface LiteCommands<SENDER> {
 
@@ -8,6 +9,9 @@ public interface LiteCommands<SENDER> {
     String VERSION = LiteCommandsVariables.VERSION;
 
     CommandManager<SENDER> getCommandManager();
+
+    @ApiStatus.Experimental
+    LiteCommandsInternal<SENDER, ?> getInternal();
 
     void register();
 
