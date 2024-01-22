3d7a185b900ed1e3ab1fd8c5cc1e3c24aa3aa446
Norbert Dejlich <ndejlich5@gmail.com>
2024-01-22 13:34:20 +0100

    GH-362 Add more java docs and jetbrains annotations. (#362)

--- litecommands-core/src/dev/rollczi/litecommands/command/CommandNode.java
+++ litecommands-core/src/dev/rollczi/litecommands/command/CommandNode.java
@@ -1,5 +1,10 @@
 package dev.rollczi.litecommands.command;
 
+/**
+ * CommandNode is an any node of the command tree.
+ * @see CommandRoute
+ * @see dev.rollczi.litecommands.command.executor.CommandExecutor
+ */
 public interface CommandNode<SENDER> {
 
     CommandRoute<SENDER> getParent();
