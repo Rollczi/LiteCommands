af97ffd4facbc91090c0c72acb72998738c5bb29
Rollczi <ndejlich5@gmail.com>
2024-02-07 12:17:11 +0100

    Separate suggester and parser for components.

--- litecommands-adventure/src/dev/rollczi/litecommands/adventure/AdventureComponentArgument.java
+++ litecommands-adventure/src/dev/rollczi/litecommands/adventure/AdventureComponentArgument.java
@@ -2,15 +2,22 @@ package dev.rollczi.litecommands.adventure;
 
 import dev.rollczi.litecommands.argument.Argument;
 import dev.rollczi.litecommands.argument.parser.ParseResult;
-import dev.rollczi.litecommands.argument.resolver.ArgumentResolver;
+import dev.rollczi.litecommands.argument.parser.Parser;
+import dev.rollczi.litecommands.input.raw.RawInput;
 import dev.rollczi.litecommands.invocation.Invocation;
+import dev.rollczi.litecommands.range.Range;
 import net.kyori.adventure.text.Component;
 
-class AdventureComponentArgument<SENDER> extends ArgumentResolver<SENDER, Component> {
+class AdventureComponentArgument<SENDER> implements Parser<SENDER, Component> {
 
     @Override
-    protected ParseResult<Component> parse(Invocation<SENDER> invocation, Argument<Component> context, String argument) {
-        return ParseResult.success(Component.text(argument));
+    public ParseResult<Component> parse(Invocation<SENDER> invocation, Argument<Component> argument, RawInput input) {
+        return ParseResult.success(Component.text(input.next()));
+    }
+
+    @Override
+    public Range getRange(Argument<Component> componentArgument) {
+        return Range.ONE;
     }
 
 }
