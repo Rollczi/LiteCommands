9c70fdd132534717561ceb131b250c98f028cdfe
Rollczi <ndejlich5@gmail.com>
2024-10-01 17:46:41 +0200

    Use TreeSet for OfflinePlayerArgument's suggestions. Add example.

--- litecommands-bukkit/src/dev/rollczi/litecommands/bukkit/argument/OfflinePlayerArgument.java
+++ litecommands-bukkit/src/dev/rollczi/litecommands/bukkit/argument/OfflinePlayerArgument.java
@@ -1,35 +1,31 @@
 package dev.rollczi.litecommands.bukkit.argument;
 
-import com.google.common.collect.Sets;
 import dev.rollczi.litecommands.argument.Argument;
 import dev.rollczi.litecommands.argument.parser.ParseResult;
 import dev.rollczi.litecommands.argument.resolver.ArgumentResolver;
-import dev.rollczi.litecommands.bukkit.LiteBukkitMessages;
 import dev.rollczi.litecommands.invocation.Invocation;
-import dev.rollczi.litecommands.message.MessageRegistry;
 import dev.rollczi.litecommands.suggestion.SuggestionContext;
 import dev.rollczi.litecommands.suggestion.SuggestionResult;
+import java.util.TreeSet;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class OfflinePlayerArgument extends ArgumentResolver<CommandSender, OfflinePlayer> {
+
+    private static final int SUGGESTION_LIMIT = 256;
 
     private final Server server;
     private final Plugin plugin;
     private final boolean enableThreadCheck;
 
-    private final Set<String> nicknames = Sets.newConcurrentHashSet();
+    private final TreeSet<String> nicknames = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
     private Instant nextWarningDate = Instant.EPOCH;
 
     public OfflinePlayerArgument(Server server, Plugin plugin, boolean enableThreadCheck) {
@@ -38,13 +34,12 @@ public class OfflinePlayerArgument extends ArgumentResolver<CommandSender, Offli
         this.enableThreadCheck = enableThreadCheck;
 
         // Server#getOfflinePlayers() can be blocking, so we don't want to call it every time
-        nicknames.addAll(
-            Arrays.stream(server.getOfflinePlayers())
-                .map(OfflinePlayer::getName)
-                .collect(Collectors.toList())
-        );
+        for (OfflinePlayer offlinePlayer : server.getOfflinePlayers()) {
+            this.nicknames.add(offlinePlayer.getName());
+        }
 
         // Save new joining player names so our suggestions are more wide
+        // TODO: Unregister this listener on Platform#unregister()
         server.getPluginManager().registerEvents(new Listener() {
             @EventHandler
             public void onPlayerJoin(PlayerJoinEvent event) {
@@ -56,8 +51,9 @@ public class OfflinePlayerArgument extends ArgumentResolver<CommandSender, Offli
     @SuppressWarnings("deprecation")
     @Override
     protected ParseResult<OfflinePlayer> parse(Invocation<CommandSender> invocation, Argument<OfflinePlayer> context, String argument) {
+        // TODO: Use async argument parsing: https://github.com/Rollczi/LiteCommands/pull/435
         if (enableThreadCheck && server.isPrimaryThread() && Instant.now().isAfter(nextWarningDate)) {
-            plugin.getLogger().warning("LiteCommands | OfflinePlayer argument resolved synchronously! This might cause server freeze!");
+            plugin.getLogger().warning("LiteCommands | OfflinePlayer argument resolved synchronously! This might cause server freeze! Add @Async to '" + context.getName() + "' argument. (command /" + invocation.name() + ")");
             nextWarningDate = Instant.now().plusSeconds(60);
         }
 
@@ -66,7 +62,21 @@ public class OfflinePlayerArgument extends ArgumentResolver<CommandSender, Offli
 
     @Override
     public SuggestionResult suggest(Invocation<CommandSender> invocation, Argument<OfflinePlayer> argument, SuggestionContext context) {
-        return SuggestionResult.of(nicknames);
+        if (nicknames.size() < SUGGESTION_LIMIT) {
+            return SuggestionResult.of(nicknames);
+        }
+
+        String input = context.getCurrent().multilevel();
+
+        if (input.isEmpty()) {
+            return nicknames.stream()
+                .limit(SUGGESTION_LIMIT)
+                .collect(SuggestionResult.collector());
+        }
+
+        return nicknames.subSet(input, input + Character.MAX_VALUE).stream()
+            .limit(SUGGESTION_LIMIT)
+            .collect(SuggestionResult.collector());
     }
 
 }
