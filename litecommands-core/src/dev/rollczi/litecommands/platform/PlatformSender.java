792185dd53dab8c47354f67929c8d9ebe60aacb3
Rollczi <ndejlich5@gmail.com>
2024-10-01 18:04:29 +0200

    Release 3.6.1

--- litecommands-core/src/dev/rollczi/litecommands/platform/PlatformSender.java
+++ litecommands-core/src/dev/rollczi/litecommands/platform/PlatformSender.java
@@ -28,7 +28,7 @@ public interface PlatformSender {
      *             Use {@link Invocation#sender()} instead and check permissions.
      */
     @Deprecated
-    @ApiStatus.ScheduledForRemoval(inVersion = "3.6.0")
+    @ApiStatus.ScheduledForRemoval(inVersion = "3.6.1")
     boolean hasPermission(String permission);
 
     <T> T getProperty(MetaKey<T> key);
