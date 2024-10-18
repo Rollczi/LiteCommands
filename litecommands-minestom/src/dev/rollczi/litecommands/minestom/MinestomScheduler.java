322be7c5f1fc188fbfa0e62f3a1d90d87900f9f6
Rollczi <ndejlich5@gmail.com>
2024-10-18 23:15:07 +0200

    Fix minestom scheduler

--- litecommands-minestom/src/dev/rollczi/litecommands/minestom/MinestomScheduler.java
+++ litecommands-minestom/src/dev/rollczi/litecommands/minestom/MinestomScheduler.java
@@ -12,43 +12,29 @@ import net.minestom.server.timer.Task;
 public class MinestomScheduler implements Scheduler {
 
     private final SchedulerManager scheduler;
-    private final Scheduler asyncScheduler;
 
-    public MinestomScheduler(SchedulerManager scheduler, Scheduler asyncScheduler) {
+    public MinestomScheduler(SchedulerManager scheduler) {
         this.scheduler = scheduler;
-        this.asyncScheduler = asyncScheduler;
     }
 
     @Override
     public <T> CompletableFuture<T> supplyLater(SchedulerPoll type, Duration delay, ThrowingSupplier<T, Throwable> supplier) {
-        SchedulerPoll poll = type.resolve(SchedulerPoll.MAIN, SchedulerPoll.ASYNCHRONOUS, MinestomSchedulerPoll.TICK_START, MinestomSchedulerPoll.TICK_END);
+        SchedulerPoll poll = type.resolve(SchedulerPoll.MAIN, SchedulerPoll.ASYNCHRONOUS);
         CompletableFuture<T> future = new CompletableFuture<>();
 
-        if (poll == SchedulerPoll.MAIN || poll == MinestomSchedulerPoll.TICK_START || poll == MinestomSchedulerPoll.TICK_END) {
-            Task.Builder built = scheduler.buildTask(() -> tryRun(supplier, future));
+        Task.Builder built = scheduler.buildTask(() -> tryRun(supplier, future))
+            .executionType(poll == SchedulerPoll.MAIN ? ExecutionType.SYNC : ExecutionType.ASYNC);
 
-            if (!delay.isZero()) {
-                built.delay(delay);
-            }
-
-            if (poll == MinestomSchedulerPoll.TICK_START) {
-                built.executionType(ExecutionType.TICK_START);
-            }
-
-            if (poll == MinestomSchedulerPoll.TICK_END) {
-                built.executionType(ExecutionType.TICK_END);
-            }
-
-            built.schedule();
-            return future;
+        if (!delay.isZero()) {
+            built.delay(delay);
         }
 
-        return asyncScheduler.supplyLater(SchedulerPoll.ASYNCHRONOUS, delay, supplier);
+        built.schedule();
+        return future;
     }
 
     @Override
     public void shutdown() {
-
     }
 
     private <T> CompletableFuture<T> tryRun(ThrowingSupplier<T, Throwable> supplier, CompletableFuture<T> future) {
