0a384210220c32a8812d488108a343b3b6f79903
Rollczi <ndejlich5@gmail.com>
2024-10-18 22:48:53 +0200

    Fix renovate. Fix farbic versions. Move farbric versions.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,37 +1,53 @@
 object Versions {
 
-    const val ADVENTURE_MINIMESSAGES = "4.17.0"
-    const val ADVENTURE_TEXT_SERIALIZER_LEGACY = "4.17.0"
-    const val ADVENTURE_PLATFORM_API = "4.3.4"
-
-    const val SPIGOT_API = "1.20.2-R0.1-SNAPSHOT"
-    const val SPIGOT = "1.8.8-R0.1-SNAPSHOT"
-    const val PROTOCOL_LIB = "5.1.0"
-
-    const val BUNGEECORD_API = "1.20-R0.2"
-
-    const val GSON = "2.11.0"
-    const val CAFFEINE = "3.1.8"
-    const val OKHTTP = "4.12.0"
-
-    const val EXPIRING_MAP = "0.5.11"
-    const val EXPRESSIBLE = "1.3.6"
+    // Core
     const val JETBRAINS_ANNOTATIONS = "24.1.0"
 
-    const val HIBERNATE_VALIDATOR = "8.0.1.Final"
-    const val EXPRESSLY = "5.0.0"
-
-    const val JDA = "5.1.2"
-
-    const val MINESTOM = "4305006e6b"
-
-    const val SPONGE_API = "8.2.0"
-
-    const val VELOCITY_API = "3.2.0-SNAPSHOT"
-
-    // tests
+    // Tests
     const val JUNIT_JUPITER = "5.11.2"
     const val ASSERTJ = "3.26.3"
     const val MOCKITO = "5.14.2"
     const val AWAITILITY = "4.2.2"
+
+    // Bukkit
+    const val SPIGOT_API = "1.20.2-R0.1-SNAPSHOT"
+    const val SPIGOT = "1.8.8-R0.1-SNAPSHOT"
+    const val PROTOCOL_LIB = "5.1.0"
+
+    // BungeeCord
+    const val BUNGEECORD_API = "1.20-R0.2"
+
+    // JDA
+    const val JDA = "5.1.2"
+
+    // Minestom
+    const val MINESTOM = "4305006e6b"
+
+    // Sponge
+    const val SPONGE_API = "8.2.0"
+
+    // Velocity
+    const val VELOCITY_API = "3.2.0-SNAPSHOT"
+
+    // Fabric
+    // check these on https://fabricmc.net/develop
+    const val FABRIC_YARN_MAPPINGS = "1.21.1+build.3"
+    val FABRIC_MINECRAFT = FABRIC_YARN_MAPPINGS.substringBefore('+')
+    const val FABRIC_LOADER = "0.16.7"
+    const val FABRIC_API = "0.106.0+1.21.1"
+
+    // ChatGPT
+    const val GSON = "2.11.0"
+    const val CAFFEINE = "3.1.8"
+    const val OKHTTP = "4.12.0"
+
+    // Adventure
+    const val ADVENTURE_MINIMESSAGES = "4.17.0"
+    const val ADVENTURE_TEXT_SERIALIZER_LEGACY = "4.17.0"
+    const val ADVENTURE_PLATFORM_API = "4.3.4"
+
+    // Jakarta
+    const val HIBERNATE_VALIDATOR = "8.0.1.Final"
+    const val EXPRESSLY = "5.0.0"
+
 }
