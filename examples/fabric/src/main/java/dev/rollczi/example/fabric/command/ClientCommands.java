cd67385a5a72a6c431d3ded842b07d21f65561ff
huanmeng-qwq <huanmeng@huanmeng-qwq.com>
2024-10-21 19:16:14 +0800

    Update example

--- examples/fabric/src/main/java/dev/rollczi/example/fabric/command/ClientCommands.java
+++ examples/fabric/src/main/java/dev/rollczi/example/fabric/command/ClientCommands.java
@@ -1,9 +1,11 @@
 package dev.rollczi.example.fabric.command;
 
+import dev.rollczi.litecommands.annotations.argument.Arg;
 import dev.rollczi.litecommands.annotations.command.Command;
 import dev.rollczi.litecommands.annotations.context.Sender;
 import dev.rollczi.litecommands.annotations.execute.Execute;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 
 @Command(name = "litecommands")
@@ -21,5 +23,10 @@ public class ClientCommands {
     @Execute(name = "time")
     String currentTime() {
         return String.valueOf(System.currentTimeMillis());
+    }
+
+    @Execute(name = "health")
+    String health(@Arg PlayerEntity player) {
+        return String.valueOf(player.getHealth());
     }
 }
