51ef7d1f3bf5b6aa14720f8cffbaa83ae9a1b343
huanmeng_qwq <huanmeng@huanmeng-qwq.com>
2024-11-13 03:52:51 +0800

    GH-485 Fix the error where Gradle can only be used normally by applying plugins on all projects (#485)
    * https://github.com/gradle/gradle/issues/17559
    
    run-task/issues/88
    
    * Fix issue with different versions.
    
    ---------
    
    Co-authored-by: Rollczi <ndejlich5@gmail.com>

--- examples/bukkit/build.gradle.kts
+++ examples/bukkit/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
     id("com.gradleup.shadow") version "8.3.5"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
-    id("xyz.jpenilla.run-paper") version "2.3.0"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 version = "3.9.1"
