48e9c12b4e766d4b9bfc82c6947bd13d24206d36
Koboo <14143908+Koboo@users.noreply.github.com>
2025-02-02 21:51:39 +0100

    GH-513 Add ConsoleOnlyContextProvider for bukkit platform (#513)

---
+++ litecommands-bukkit/src/dev/rollczi/litecommands/bukkit/context/ConsoleOnlyContextProvider.java
@@ -0,0 +1,29 @@
+package dev.rollczi.litecommands.bukkit.context;
+
+import dev.rollczi.litecommands.bukkit.LiteBukkitMessages;
+import dev.rollczi.litecommands.context.ContextProvider;
+import dev.rollczi.litecommands.context.ContextResult;
+import dev.rollczi.litecommands.invocation.Invocation;
+import dev.rollczi.litecommands.message.MessageRegistry;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+public class ConsoleOnlyContextProvider implements ContextProvider<CommandSender, ConsoleCommandSender> {
+
+    private final MessageRegistry<CommandSender> messageRegistry;
+
+    public ConsoleOnlyContextProvider(MessageRegistry<CommandSender> messageRegistry) {
+        this.messageRegistry = messageRegistry;
+    }
+
+    @Override
+    public ContextResult<ConsoleCommandSender> provide(Invocation<CommandSender> invocation) {
+        if (invocation.sender() instanceof ConsoleCommandSender) {
+            return ContextResult.ok(() -> (ConsoleCommandSender) invocation.sender());
+        }
+
+        return ContextResult.error(messageRegistry.getInvoked(LiteBukkitMessages.CONSOLE_ONLY, invocation));
+    }
+
+}
