61cce11234629924b8df8a3bfa727f2552d6b7d0
Rollczi <ndejlich5@gmail.com>
2025-04-21 17:58:54 +0200

    Update gradle

--- examples/telegrambots/src/main/java/dev/rollczi/example/telegrambots/command/TellCommand.java
+++ examples/telegrambots/src/main/java/dev/rollczi/example/telegrambots/command/TellCommand.java
@@ -1,8 +1,11 @@
 package dev.rollczi.example.telegrambots.command;
 
 import dev.rollczi.litecommands.annotations.command.Command;
+import dev.rollczi.litecommands.annotations.context.Context;
 import dev.rollczi.litecommands.annotations.context.Sender;
 import dev.rollczi.litecommands.annotations.execute.Execute;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 
 @Command(name = "tell")
@@ -11,6 +14,15 @@ public class TellCommand {
     @Execute
     String executeTell(@Sender User user) {
         return user.getFirstName() + " " + user.getLastName() + " told me that you are a good person!";
+    }
+
+    @Execute(name = "more")
+    SendMessage more(@Context Update update) {
+        return SendMessage
+            .builder()
+            .chatId(update.getMessage().getChatId())
+            .chatId("We used SendMessage object to send this message!")
+            .build();
     }
 
 }
