164bbaed87097345752a56fcf46570ce2ad5b424
Rollczi <ndejlich5@gmail.com>
2025-06-22 16:40:59 +0200

    Update renovate

--- examples/fabric/build.gradle.kts
+++ examples/fabric/build.gradle.kts
@@ -17,8 +17,10 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.21.1")
-    mappings("net.fabricmc:yarn:1.21.1+build.3")
+    val yarnVersion = "1.21.1+build.3"
+    val minecraftVersion = yarnVersion.substringBefore('+')
+    mappings("net.fabricmc:yarn:$yarnVersion")
+    minecraft("com.mojang:minecraft:$minecraftVersion")
 
     modImplementation("net.fabricmc:fabric-loader:0.16.7")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.106.0+1.21.1")
