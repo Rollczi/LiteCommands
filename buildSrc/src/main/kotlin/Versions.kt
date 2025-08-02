de92e5536e68b20d000f71b4239b27a24fbe18fb
Rollczi <ndejlich5@gmail.com>
2025-08-02 16:18:51 +0200

    Bump velocity and add warning for Folia

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,7 +12,6 @@ object Versions {
     // Bukkit
     const val SPIGOT_API = "1.20.2-R0.1-SNAPSHOT"
     const val FOLIA_API = "1.21.4-R0.1-SNAPSHOT"
-    const val SPIGOT = "1.8.8-R0.1-SNAPSHOT"
     const val PROTOCOL_LIB = "5.3.0"
 
     // BungeeCord
@@ -28,7 +27,7 @@ object Versions {
     const val SPONGE_API = "13.0.0"
 
     // Velocity
-    const val VELOCITY_API = "3.2.0-SNAPSHOT"
+    const val VELOCITY_API = "3.4.0-SNAPSHOT"
 
     // Fabric
     // check these on https://fabricmc.net/develop
