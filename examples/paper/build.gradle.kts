ebf4bec09b57e40abc2ba06a9fae0016d568381d
Rollczi <Rollczi@users.noreply.github.com>
2026-01-13 23:43:17 +0000

    Release 3.10.8

--- examples/paper/build.gradle.kts
+++ examples/paper/build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
 
-    // implementation("dev.rollczi:litecommands-bukkit:3.10.7") // <-- uncomment in your project
+    // implementation("dev.rollczi:litecommands-bukkit:3.10.8") // <-- uncomment in your project
     implementation(project(":litecommands-bukkit")) // don't use this line in your build.gradle
 }
 
