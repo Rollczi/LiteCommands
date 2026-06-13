9929e5ffbe3e1ddc9daf3b37211b6df4a1e20034
Michał Łuczak <contact@michal-luczak.pl>
2026-06-13 17:46:52 +0200

    feat: Add javadocs to @CaseInsensitive annotation

--- litecommands-annotations/src/dev/rollczi/litecommands/annotations/argument/CaseInsensitive.java
+++ litecommands-annotations/src/dev/rollczi/litecommands/annotations/argument/CaseInsensitive.java
@@ -7,6 +7,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates that an enum parameter's value should be treated in a case-insensitive manner.
+ * <p>
+ * This annotation is experimental and may be subject to change.
+ */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @ApiStatus.Experimental
